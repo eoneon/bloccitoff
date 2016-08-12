850ecb024cc9fdf434ab14b50e88992a2fc43485
Eon <eoneon123@gmail.com>
2016-08-12 15:20:19 -0700

    updated seed, created RandomData, g'd UserController, users#show, new root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
 
   get '/token' => 'home#token', as: :token
 
   resources :home, only: :index
 
-  root "home#index"
+  root 'users#show'
 end
