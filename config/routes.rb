805a474f7b5ad8cf990f0fc553950003bf528a39
Eon <eoneon123@gmail.com>
2016-08-15 08:03:16 -0700

    added: item model, controller, partials to create/display items + updated user controller to redirect back (to root)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'items/create'
+
   get 'users/show'
 
   devise_for :users
+
+  resources :users, only: [] do
+    resources :items, only: [:create]
+  end
 
   get '/token' => 'home#token', as: :token
 
