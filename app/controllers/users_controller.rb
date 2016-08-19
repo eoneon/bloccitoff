2ac5fef7014bdf4ae0d1c38cf4db52fe5cd0cf0e
Eon <eoneon123@gmail.com>
2016-08-18 20:50:19 -0700

    private-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authorize_user, only: :show
+
   def show
     @user = current_user
     @items = @user.items
+  end
+
+  private
+
+  def authorize_user
+    user = User.find(params[:id])
+    unless current_user == user
+      flash[:alert] = "This view is private"
+      redirect_to :home_index
+    end
   end
 end
